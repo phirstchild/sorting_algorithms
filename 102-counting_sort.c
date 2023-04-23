5a723a5f7754e888a92293859f982c43f71d147a
phirstchild <117740003+phirstchild@users.noreply.github.com>
2023-04-23 15:27:44 +0100

    task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -4,9 +4,9 @@
  * get_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
+ * Return:The maximum integer in the array.
  */
+
 int get_max(int *array, int size)
 {
 	int max, i;
